559604ec57c42071a2fbee21a6928a6375987c0a
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-01-17 18:04:23 +0100

    generic_a15: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: huawei Model: generic_a15 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_generic_a15.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_generic_a15-user \
+    omni_generic_a15-userdebug \
+    omni_generic_a15-eng
