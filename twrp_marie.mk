609f41ee8ce22d23417e8829bb0a4e6956d29d94
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-17 18:09:40 +0100

    Update device and product names for TWRP marie

--- twrp_marie.mk
+++ twrp_marie.mk
@@ -9,16 +9,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from generic_a15 device
-$(call inherit-product, device/huawei/generic_a15/device.mk)
+# Inherit from marie device
+$(call inherit-product, device/huawei/marie/device.mk)
 
-PRODUCT_DEVICE := generic_a15
-PRODUCT_NAME := omni_generic_a15
+PRODUCT_DEVICE := marie
+PRODUCT_NAME := twrp_marie
 PRODUCT_BRAND := Huawei
-PRODUCT_MODEL := generic_a15
+PRODUCT_MODEL := twrp_marie
 PRODUCT_MANUFACTURER := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
