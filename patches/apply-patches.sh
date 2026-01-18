b66a9abe72b88594899e6b2f6bb8797dd73a9727
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-18 13:48:57 +0100

    Disable patch application in apply-patches.sh
    Comment out the patch application commands in the script.

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-cd external/f2fs-tools
-git apply -v ../../device/huawei/marie/patches/0001-f2fs-tools-remove-CP_NAT_BITS_FLAG.patch
-cd ../..
+# Don't apply anything for now.
+# cd external/f2fs-tools
+# git apply -v ../../device/huawei/marie/patches/0001-f2fs-tools-remove-CP_NAT_BITS_FLAG.patch
+# cd ../..
