3d0c90a1e453e7cbb2491bcc88c98b0865607347
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-18 10:10:05 +0100

    Add FastbootD configuration to device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,17 @@
 #
 
 LOCAL_PATH := device/huawei/marie
+
+# FastbootD
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fastbootd.available=true \
+    ro.boot.dynamic_partitions_retrofit=true \
+    ro.boot.dynamic_partitions=true
