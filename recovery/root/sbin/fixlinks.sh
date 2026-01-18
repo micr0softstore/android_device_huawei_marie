a6c2218775a37a9f3cd7d06a86cc9fb5f6de3234
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-18 10:05:57 +0100

    Update fixlinks.sh

--- recovery/root/sbin/fixlinks.sh
+++ recovery/root/sbin/fixlinks.sh
@@ -1,17 +1,9 @@
 #!/sbin/sh
 
-if [ -d /dev/block/platform/ff3c0000.ufs ]; then
-    echo "using ufs"
-    ln -sf /dev/block/platform/ff3c0000.ufs /dev/block/bootdevice
-else
-    echo "using emmc"
-    ln -sf /dev/block/platform/hi_mci.0 /dev/block/bootdevice
-fi
+echo "Linking mountpoints to /dev/block/bootdevice"
+mkdir /dev/block/bootdevice
+ln -sf /dev/block/platform/hi_mci.0/* /dev/block/bootdevice
+rm -rf /dev/block/bootdevice/by-name
+mkdir /dev/block/bootdevice/by-name
+ln -sf /dev/block/platform/hi_mci.0/by-name/* /dev/block/bootdevice/by-name
 
-# Creates new symlinks without the _a in them since this device does
-# not really have boot slots (silly Huawei...)
-
-for f in /dev/block/bootdevice/by-name/*_a
-do
-	ln -sf $f ${f%??}
-done
