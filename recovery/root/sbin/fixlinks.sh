1eec7b8a91cb157fc0bcf9e02542213aa116749e
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-19 00:19:16 +0100

    Delete recovery/root/sbin/fixlinks.sh

--- recovery/root/sbin/fixlinks.sh
+++
@@ -1,9 +0,0 @@
-#!/sbin/sh
-
-echo "Linking mountpoints to /dev/block/bootdevice"
-mkdir /dev/block/bootdevice
-ln -sf /dev/block/platform/hi_mci.0/* /dev/block/bootdevice
-rm -rf /dev/block/bootdevice/by-name
-mkdir /dev/block/bootdevice/by-name
-ln -sf /dev/block/platform/hi_mci.0/by-name/* /dev/block/bootdevice/by-name
-
