0fe4bd629689d5fd4140cc42ae656e60ec984196
Microsoft Store (Balazs) <micr0softstore@outlook.com>
2026-01-18 11:06:52 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,7 +60,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Dynamic partitions (super)
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
 BOARD_SUPER_PARTITION_SIZE := 5771493376 # 5.5GB (5771362304+131072)
 BOARD_SUPER_PARTITION_GROUPS := huawei_dynamic_partitions
 BOARD_HUAWEI_DYNAMIC_PARTITIONS_PARTITION_LIST := odm product hw_product system system_ext vendor cust
